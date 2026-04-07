81e63ac76917938d536e2678efdee0b9c6144780
maryamnibras157 <maryamnibras.s@gmail.com>
2026-04-07 11:09:23 +0530

    Update Flask app to use environment variable for port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import random
+import os
 
 app = Flask(__name__)
 
@@ -203,5 +204,5 @@ def reset_settings():
     settings = default_settings.copy()
     return jsonify({"message": "Settings reset", "settings": settings})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
